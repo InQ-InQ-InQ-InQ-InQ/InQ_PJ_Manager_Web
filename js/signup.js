9cbe2cda1bae06e744cc992a307e72e63fc91096
soi-ha <soy2302ten@gmail.com>
2022-05-10 16:47:54 +0900

    home html 작성

--- js/signup.js
+++ js/signup.js
@@ -1,7 +1,7 @@
-const openBtn = document.getElementById(".btn")
-fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
-  .then(response => response.text())
-  .then(result => openBtn.innerHTML = result)
+const loremIpsum = document.getElementById("lorem-ipsum")
+  fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
+    .then(response => response.text())
+    .then(result => loremIpsum.innerHTML = result)
 
 const modal = document.getElementById("modal")
 const btnModal = document.getElementById("btn-modal")
