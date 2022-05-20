973760082439e05d4e00a7425e7138ddd28ac63c
eunchannam <namhm23@gmail.com>
2022-05-20 11:09:52 +0900

    project 등록시 본인 자동참여 로직 추가

--- src/main/java/team/projectmanager/domain/project/projectservice/ProjectServiceImp.java
+++ src/main/java/team/projectmanager/domain/project/projectservice/ProjectServiceImp.java
@@ -3,6 +3,10 @@ package team.projectmanager.domain.project.projectservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.projectmanager.domain.member.Member;
+import team.projectmanager.domain.member.memberservice.MemberService;
+import team.projectmanager.domain.memberproject.MemberProject;
+import team.projectmanager.domain.memberproject.mprepository.MemberProjectRepository;
 import team.projectmanager.domain.position.Position;
 import team.projectmanager.domain.project.Project;
 import team.projectmanager.domain.project.projectrepository.ProjectRepository;
@@ -15,10 +19,17 @@ import java.util.List;
 public class ProjectServiceImp implements ProjectService {
 
     private final ProjectRepository pr;
+    private final MemberService ms;
+    private final MemberProjectRepository mpr;
 
     @Transactional
     public void newProject(Long memberId, String name, LocalDate period, LocalDate startDate, LocalDate endDate, String introduction, List<Position> positions) {
+
         Project project = Project.createProject(memberId, name, period, startDate, endDate, introduction, positions);
         pr.save(project);
+
+        Member member = ms.findById(memberId);
+        MemberProject memberProject = MemberProject.createMemberProject(member, project);
+        mpr.save(memberProject);
     }
 }
